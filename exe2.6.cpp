a7202ab2a8c08c9b8e6d17bb1c125424787b52a8
Aqib Nazir <108110264+Aqib313-max@users.noreply.github.com>
2023-05-08 19:19:52 +0530

    Add files via upload

---
+++ exe2.6.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<iomanip>
+using  namespace std;
+int main()
+{
+	float br,us,fr,gr,jp;
+	cout<<"enter the british amount"<<endl;
+	cin>>br;
+	us=br*1.487;
+	fr=br*0.172;
+	gr=br*0.584;
+	jp=br*0.0095;
+	cout<<"the  amount =%f"<<us<<endl;
+    cout<<"the  amount =%f"<<fr<<endl;
+    cout<<"the  amount =%f"<<gr<<endl;
+    cout<<"the  amount =%f"<<jp<<endl;
+    
+}
