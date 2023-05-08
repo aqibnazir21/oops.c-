fa1192dc170eb4f7f43a92c8f95c0a336b19b0ab
Aqib Nazir <108110264+Aqib313-max@users.noreply.github.com>
2023-05-08 19:16:52 +0530

    Add files via upload

---
+++ calling main function.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+int main();
+int display(int);
+int main()
+{
+	int x;
+//	printf(" x= \n");
+	//scanf("%d",x);
+	int c;
+	c= display(c);
+	printf("learn c=%d\n",c);
+	return 0;
+}                              //the output will be infinite
+int display(int c)               
+{
+	int x;
+    if(x>=45)
+    return (100); 
+    else
+    return(x=10);
+	return c;
+	
+}
