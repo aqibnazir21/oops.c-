1940faa84c05cb145a1eabf19c784be1d120bae6
Aqib Nazir <108110264+Aqib313-max@users.noreply.github.com>
2023-05-08 18:40:38 +0530

    Add files via upload

---
+++ class 2.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+class fact
+{
+	private:
+		int nim1;
+	public:
+	void input()
+	{
+		int num1;
+		cout<<"enter any number";
+		cin>>num1;
+	}
+	int factorial()
+	{
+		int i,f,num1;
+		while(i<=num1);
+		{
+			i=1;
+			f=num1*i;
+			i++;
+		}
+		cout<<f;
+	}	  	
+};
+int main()
+{
+	fact n1;
+	n1.input();
+	n1.factorial();
+}
