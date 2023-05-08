473cb890728336edbf372e362cd8dac66788dc18
Aqib Nazir <108110264+Aqib313-max@users.noreply.github.com>
2023-05-08 18:44:19 +0530

    Add files via upload

---
+++ exe.2.2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<iomanip>
+using  namespace std;
+int main()
+{
+//	char a;
+	cout<<setfill('.');
+	cout<<setw(4)<<"1990"<<setw(9)<<"135"<<endl;
+	cout<<setw(4)<<"1991"<<setw(9)<<"7290"<<endl;
+	cout<<setw(4)<<"1992"<<setw(9)<<"11300"<<endl;
+	cout<<setw(4)<<"1993"<<setw(9)<<"16200"<<endl;
+}
